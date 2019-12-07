ef3378c99e2b874f6406e51d74adc71c3d7d34a6
AsaGayle <azmatch.gayle@gmail.com>
2019-12-07 02:49:40 -0500

    Added walkthrough GIF

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -26,7 +26,7 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    private static final String YOUTUBE_API_KEY = "AIzaSyAm8nb-4Uk13rF5ppPnxDycXqHuNy-6G6c";
+    private static final String YOUTUBE_API_KEY = "";
     public static final String BASE_API_REQUEST = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     //Example ending to API Request
     //209112/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed
